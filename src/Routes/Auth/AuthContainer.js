a8b5e1251544cee0b82085404ef34df85f4af754
SeokHyun Cho <doongdang@naver.com>
2020-04-07 15:53:59 +0900

    #5.5

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import useInput from "../../Hooks/useInput";
 import AuthPresenter from "./AuthPresenter";
 import { useMutation } from "react-apollo-hooks";
-import { LOG_IN } from "./AuthQueries";
+import { LOG_IN, CREATE_ACCOUNT } from "./AuthQueries";
+import { toast } from "react-toastify";
 
 export default () => {
   const [action, setAction] = useState("LogIn");
@@ -11,13 +12,44 @@ export default () => {
   const lastName = useInput("");
   const email = useInput("");
   const [requestSecret] = useMutation(LOG_IN, {
+    update: (_, { data }) => {
+      const { requestSecret } = data;
+      if (!requestSecret) {
+        toast.error("Don`t have an Account yet. Create New Account.");
+        setTimeout(() => setAction("signup"), 1500);
+      }
+    },
     variables: { email: email.value },
   });
 
-  const onLogin = (e) => {
+  const [createAccount] = useMutation(CREATE_ACCOUNT, {
+    variables: {
+      email: email.value,
+      username: username.value,
+      firstName: firstName.value,
+      lastName: lastName.value,
+    },
+  });
+
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (email !== "") {
-      requestSecret();
+    if (action === "logIn") {
+      if (email.value !== "") {
+        requestSecret();
+      } else {
+        toast.error("Email is Required");
+      }
+    } else if (action === "signUp") {
+      if (
+        email.value !== "" &&
+        username.value !== "" &&
+        firstName.value !== "" &&
+        lastName.value !== ""
+      ) {
+        createAccount();
+      } else {
+        toast.error("All Fields are Required.");
+      }
     }
   };
 
@@ -29,7 +61,7 @@ export default () => {
       firstName={firstName}
       lastName={lastName}
       email={email}
-      onLogin={onLogin}
+      onSubmit={onSubmit}
     />
   );
 };
